e1d9406ada6fbd421055a8eb30ef2e2eb05b7244
Cristhian Mora <linkincypher@hotmail.com>
2024-06-18 11:16:06 -0500

    modelo y relaciones

--- src/main/java/com/uisrael/acmemoda/modelo/Cliente.java
+++ src/main/java/com/uisrael/acmemoda/modelo/Cliente.java
@@ -1,5 +1,43 @@
 package com.uisrael.acmemoda.modelo;
 
-public class Cliente {
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idCliente;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String direccion;
+	private boolean estado;
+
+	@Column(name = "cedula", length = 15)
+	private String ci;
+
+	@Column(length = 10)
+	private String telefono;
+
+	@OneToMany(mappedBy = "fkCliente")
+	private List<Pedido> listaPedidos = new ArrayList<>();
 }
--- src/main/java/com/uisrael/acmemoda/modelo/Pedido.java
+++ src/main/java/com/uisrael/acmemoda/modelo/Pedido.java
@@ -1,5 +1,35 @@
 package com.uisrael.acmemoda.modelo;
 
-public class Pedido {
+import java.io.Serializable;
+import java.sql.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_pedido")
+public class Pedido implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idPedido;
+	private boolean estado;
+
+	@Temporal(TemporalType.DATE)
+	private Date fecha;
+
+	@ManyToOne
+	@JoinColumn(name = "id_Cliente")
+	private Cliente fkCliente;
 }
