17a1c6b4b517adc5c902667a2d71285c045a2e80
Cristhian Mora <linkincypher@hotmail.com>
2024-06-18 09:45:01 -0500

    Inicio
    Generar Spring en https://start.spring.io/
    - PostgreSQL Driver
    - Spring Data JPA
    - Lombok
    - Spring Web
    - Thymeleaf

---
+++ src/test/java/com/uisrael/acmemoda/AcmemodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.acmemoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmemodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
