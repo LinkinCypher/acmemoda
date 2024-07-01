e679b3ebcc8c5c3e7cf98fac7cc9b4d9d8ee46ba
Cristhian Casa <77740895+LinkinCypher@users.noreply.github.com>
2024-07-01 18:57:04 -0500

    Agregando Pedido

--- src/main/java/com/uisrael/acmemoda/controlador/ClienteControlador.java
+++ src/main/java/com/uisrael/acmemoda/controlador/ClienteControlador.java
@@ -6,24 +6,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ClienteControlador {
 	
-	@GetMapping("/cliente")
-	public String crearCursos() {
-		return "/material/cliente";
-	}
-	
-	@GetMapping("/auth-normal-sign-in")
-    public String showLoginPage() {
-        return "/material/auth-normal-sign-in"; // Thymeleaf buscará auth-normal-sign-in en src/main/resources/templates
+	@GetMapping("/ingreso")
+    public String verLogin() {
+        return "/material/ingreso"; // Thymeleaf buscará auth-normal-sign-in en src/main/resources/templates
     }
 
-    @GetMapping("/auth-sign-up")
-    public String showRegistrationPage() {
-        return "/material/auth-sign-up"; // Thymeleaf buscará auth-sign-up en src/main/resources/templates
+    @GetMapping("/registro")
+    public String verRegistro() {
+        return "/material/registro"; // Thymeleaf buscará auth-sign-up en src/main/resources/templates
     }
     
     @GetMapping("/index")
-    public String showIndexPage() {
+    public String verIndex() {
         return "/material/index"; // Thymeleaf buscará index en src/main/resources/templates
+    }
+    
+    @GetMapping("/usuario")
+	public String verCliente() {
+		return "/material/usuario";
+	}
+    
+    @GetMapping("/pedido")
+    public String verPedido() {
+        return "/material/pedido"; // Thymeleaf buscará index en src/main/resources/templates
     }
 
 }
