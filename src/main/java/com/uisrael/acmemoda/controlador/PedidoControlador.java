1c4809798f3fc5b6365fd3c16a359570ac4c1ae8
Cristhian Mora <linkincypher@hotmail.com>
2024-07-15 16:59:25 -0500

    update

--- src/main/java/com/uisrael/acmemoda/controlador/PedidoControlador.java
+++ src/main/java/com/uisrael/acmemoda/controlador/PedidoControlador.java
@@ -43,7 +43,7 @@ public class PedidoControlador {
     
     
     
-    //GUardar -> pedido-registro
+    //Guardar -> pedido-registro
     @PostMapping("/insertarpedido")
     public String guardarpedido(@ModelAttribute("nuevoPedido") Pedido nuevoPedido) {
     	servicioPedido.insertarPedido(nuevoPedido);
